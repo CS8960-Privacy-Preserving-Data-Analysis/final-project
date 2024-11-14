ca98b94f16619621ca17549bea696bb6226c2444
padrejoe <59787022+alexander-darwiche@users.noreply.github.com>
2024-11-13 21:14:20 -0500

    Revert per sample grad implementation

--- src/trainer.py
+++ src/trainer.py
@@ -328,22 +328,6 @@ def train(train_loader, model, criterion, optimizer, epoch, privacy_engine=None)
     # switch to train mode
     model.train()
 
-    fmodel, _fparams = make_functional(model)
-
-    def compute_loss_stateless_model(params, sample, target):
-        batch = sample.unsqueeze(0)
-        targets = target.unsqueeze(0)
-
-        predictions = fmodel(params, batch)
-        loss = criterion(predictions, targets)
-        return loss
-    
-    ft_compute_grad = grad_and_value(compute_loss_stateless_model)
-    ft_compute_sample_grad = vmap(ft_compute_grad, in_dims=(None, 0, 0))
-    # Using model.parameters() instead of fparams
-    # as fparams seems to not point to the dynamically updated parameters
-    params = list(model.parameters())
-
     end = time.time()
     for i, (input, target) in enumerate(train_loader):
         import pdb;pdb.set_trace()
@@ -359,19 +343,10 @@ def train(train_loader, model, criterion, optimizer, epoch, privacy_engine=None)
         # compute output
         output = model(input_var)
 
-        per_sample_grads, per_sample_losses = ft_compute_sample_grad(
-            params, input_var, target_var
-        )
-        per_sample_grads = [g.detach() for g in per_sample_grads]
-        loss = torch.mean(per_sample_losses)
-        for p, g in zip(params, per_sample_grads):
-            p.grad_sample = g
-
-
         loss = criterion(output, target_var)
 
         # compute gradient and do SGD step
-        # loss.backward()
+        loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
